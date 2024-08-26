79a2b545a78d2d1aad42dbd99ad8436c00ec59bf
Jegatheeswari.K <jegatheeswarimkovil@gmail.com>
2024-08-26 15:48:20 +0530

    one file updated

--- AuthenticationPage.java
+++ AuthenticationPage.java
@@ -1,5 +1,6 @@
 class AuthenticationPage{
     public static void main(String[] args) {
         System.out.println("This is an Authentication page");
+        System.out.println("This is an Authentication");
     }
 }
